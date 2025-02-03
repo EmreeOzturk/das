f36dc5c3568f08ca88f7b9a0ee3444e57eb8bd76
Emre Ozturk <emreeozturk42@gmail.com>
2025-02-03 08:51:12 +0300

    Add site configuration constants for main navigation

---
+++ lib/constants.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "Your Site Name",
+  description: "Your site description",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ],
+}; 
